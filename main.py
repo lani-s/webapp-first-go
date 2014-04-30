8920e7ac1dc20b019ae19d78d76f7f785697a0bf
Lani Shamash <your-email-address>
2014-04-30 12:36:35 +0100

    edit

--- main.py
+++ main.py
@@ -21,15 +21,19 @@ form="""
 <form method="post">
     What is your birthday?
     <br>
-    <label> Month
-        <input type="text" name="month">
+    <label> 
+        Month
+        <input type="text" name="month" value="%(month)s">
     </label>
-    <label> Day
-        <input type="text" name="day">
+    <label> 
+        Day
+        <input type="text" name="day" value="%(day)s">
     </label>
-    <label> Year
-        <input type="text" name="year">
+    <label> 
+        Year
+        <input type="text" name="year" value="%(year)s">
     </label>
+    <div style="color:red">%(error)s</div>
     <br>
     <br>
 	<input type = "submit">
@@ -37,21 +41,29 @@ form="""
 """
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error,
+                                        "month": month,
+                                        "day": day,
+                                        "year": year})
+
     def get(self):
-        self.response.write(form)
+        self.write_form()
 
 
     def post(self):
-        #other_funcs.valid_month(month)
-        #other_funcs.valid_day(day)
-        #other_funcs.valid_year(year)
 
-        user_month = other_funcs.valid_month(self.request.get('month'))
-        user_day = other_funcs.valid_day(self.request.get('day'))
-        user_year = other_funcs.valid_year(self.request.get('year'))
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
 
-        if not (user_month and user_day and user_year):
-            self.response.out.write(form)
+        month = other_funcs.valid_month(user_month)
+        day = other_funcs.valid_day(user_day)
+        year = other_funcs.valid_year(user_year)
+
+        if not (month and day and year):
+            self.write_form("That doesn't look right to me, friend.",
+                            user_month, user_day, user_year)
         else:
             self.response.out.write("Thanks! That's a valid day!")
 
